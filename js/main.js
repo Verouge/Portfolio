d4eef0200f4df5f8b5efdc3cb04226d77c9de4af
Verouge <branola1998628@gmail.com>
2023-06-06 17:19:45 +1000

    Completed Hero section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 })
+
+document.querySelectorAll('.nav-link').forEach(n => n. addEventListener('click', () => {
+    menu_btn.classList.remove('is-active');
+    mobile_menu.classList.remove('is-active');
+}))
